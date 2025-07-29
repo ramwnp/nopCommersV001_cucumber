6af17c9af690c2c79c705948e12ea7d565a56bdc
RAM <ravigannoj44@gmail.com>
2025-07-29 16:36:53 +0530

    print statement deleted

--- src/test/java/pageObjects/SearchCustomerPage.java
+++ src/test/java/pageObjects/SearchCustomerPage.java
@@ -111,7 +111,6 @@ public class SearchCustomerPage {
         return tablecolumns.size();
     }
     public boolean searchCustomersByEmailID(String email){
-        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$"+email);
         boolean flag=false;
         for (int i = 1; i <= getNoOfRows(); i++) {
             String emailId = table.findElement(By.xpath("//table[@id='customers-grid']/tbody/tr["+i+"]/td[2]")).getText();
